4ad83e3fd743272dcf27e2567c6335e0722e03ed
TakuroBreath <133218511+TakuroBreath@users.noreply.github.com>
2023-09-21 17:03:43 +0300

    Added sql-script

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
--- SQL-команды для создания таблиц
+CREATE TABLE customers_data
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(200) NOT NULL,
+	contact_name varchar(200) NOT NULL
+);
+
+CREATE TABLE employees_data
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(200) NOT NULL,
+	birth_date varchar(100) NOT NULL,
+	notes text
+);
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers_data(customer_id) NOT NULL,
+	employee_id int REFERENCES employees_data(employee_id) NOT NULL,
+	order_date varchar(100) NOT NULL,
+	ship_city varchar(100) NOT NULL
+)-- SQL-команды для создания таблиц
