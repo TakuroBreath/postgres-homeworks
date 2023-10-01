c4db9b7b2e4c2053b17866b5e1b556ba8ee0d1e8
TakuroBreath <133218511+TakuroBreath@users.noreply.github.com>
2023-10-01 17:31:21 +0300

    Update alter_student.sql

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,31 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+CREATE TABLE student
+(
+student_id serial,
+first_name varchar,
+last_name varchar,
+birthday date,
+phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student RENAME birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES
+  ('Max', 'Takuro', '2003-05-12', '260-820-230'),
+  ('Ivan', 'Chak', '1656-11-11', '027-324-573'),
+  ('Abey', 'Belgium', '2001-09-11', '356-934-666');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
